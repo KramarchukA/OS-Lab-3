aeb95f072cbac4655c42822a581340d51bbfd0a0
KramarchukA <135083063+KramarchukA@users.noreply.github.com>
2025-01-08 15:11:13 +0500

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/anita/OSlabs/Lab3/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/anita/OSlabs/Lab3/Module.symvers -T /home/anita/OSlabs/Lab3/modules.order -i Module.symvers -e 
