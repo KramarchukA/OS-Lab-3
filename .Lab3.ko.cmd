5a28480dcf39dd92876f4002830c603d1736364b
KramarchukA <135083063+KramarchukA@users.noreply.github.com>
2025-01-08 15:10:13 +0500

    Delete .Lab3.ko.cmd

--- .Lab3.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/anita/OSlabs/Lab3/Lab3.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T scripts/module.lds -o /home/anita/OSlabs/Lab3/Lab3.ko /home/anita/OSlabs/Lab3/Lab3.o /home/anita/OSlabs/Lab3/Lab3.mod.o
